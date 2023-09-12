a30948af4337dbd11ce39de5a4f12ed455771147
kevinlawrenceking <kevinking7135@gmail.com>
2023-09-11 17:59:08 -0700

    dashboard

--- app/assets/js/editmode.js
+++ app/assets/js/editmode.js
@@ -1,17 +1,29 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-  // ... Your other code for packery and stuff ...
-  
+  // Debugging: check if DOMContentLoaded is firing
+  console.log('DOMContentLoaded fired');
+
   const editModeButtons = document.querySelectorAll(".toggle_edit_mode");
   
+  // Debugging: print out the NodeList of buttons
+  console.log('Edit Mode Buttons:', editModeButtons);
+
   editModeButtons.forEach((button) => {
     button.addEventListener("click", function(e){
       e.preventDefault();
       
+      // Debugging: print out which button was clicked
+      console.log('Button clicked:', e.currentTarget);
+
       const cardId = e.currentTarget.dataset.cardId;
       
-      // Note: We're using a query selector that matches the data-card-id to the cardId
+      // Debugging: print out the cardId
+      console.log('Card ID:', cardId);
+
       const editIcons = document.querySelectorAll(`[data-card-id="${cardId}"]`);
       
+      // Debugging: print out the NodeList of matching edit icons
+      console.log('Edit Icons:', editIcons);
+
       editIcons.forEach((icon) => {
         icon.classList.toggle("hide-edit-icon");
       });
